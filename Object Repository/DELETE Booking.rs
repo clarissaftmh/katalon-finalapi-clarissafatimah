d37e3e5a966a453adef356676c23a4e2a4de9b16
clarissaftmh <clarissafatimah@gmail.com>
2024-02-13 15:04:33 +0700

    deletebooking

---
+++ Object Repository/DELETE Booking.rs
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>DELETE Booking</name>
+   <tag></tag>
+   <elementGuidId>b19ac66f-8294-46b5-82b2-a09c9329834f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>true</autoUpdateContent>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>token = 3f707d4521ea6e2</value>
+      <webElementGuid>cad3fc4c-929c-4744-bc2e-b8453b03cae9</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>745604a3-afd5-4be3-974e-0b87d8e49e60</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>9.3.0</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>DELETE</restRequestMethod>
+   <restUrl>${GlobalVariable.url}/booking/495</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
--- Object Repository/Put Booking.rs
+++ Object Repository/Put Booking.rs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>Put Booking - UpdateBooking</name>
+   <name>Put Booking</name>
    <tag></tag>
    <elementGuidId>72882e63-bb0b-4d72-b0ca-ba5c49ed8711</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
@@ -28,7 +28,7 @@
    <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>PUT</restRequestMethod>
-   <restUrl>${GlobalVariable.url}/booking/1</restUrl>
+   <restUrl>${GlobalVariable.url}/booking/2276</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
@@ -37,6 +37,48 @@
    <soapServiceFunction></soapServiceFunction>
    <socketTimeout>-1</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'clarissa'</defaultValue>
+      <description></description>
+      <id>0fac9f96-528d-4209-a636-5f231d409c07</id>
+      <masked>false</masked>
+      <name>firstname</name>
+   </variables>
+   <variables>
+      <defaultValue>'fatimah'</defaultValue>
+      <description></description>
+      <id>93298795-2eb8-4097-8609-f057b8bb9dcb</id>
+      <masked>false</masked>
+      <name>lastname</name>
+   </variables>
+   <variables>
+      <defaultValue>'100'</defaultValue>
+      <description></description>
+      <id>af1db5ef-dbf9-4541-8544-f98c91bbc2ee</id>
+      <masked>false</masked>
+      <name>totalprice</name>
+   </variables>
+   <variables>
+      <defaultValue>'true'</defaultValue>
+      <description></description>
+      <id>eb7a17af-f165-4287-b503-f4b234891a50</id>
+      <masked>false</masked>
+      <name>depositpaid</name>
+   </variables>
+   <variables>
+      <defaultValue>'2024-02-10'</defaultValue>
+      <description></description>
+      <id>41843ca6-00c9-490a-ab20-bee2d7cde958</id>
+      <masked>false</masked>
+      <name>checkin</name>
+   </variables>
+   <variables>
+      <defaultValue>'2024-02-19'</defaultValue>
+      <description></description>
+      <id>99a839c9-e8ff-4066-9a69-dfbd12aee3cd</id>
+      <masked>false</masked>
+      <name>checkout</name>
+   </variables>
    <verificationScript>import static org.assertj.core.api.Assertions.*
 
 import com.kms.katalon.core.testobject.RequestObject
